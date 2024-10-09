f28788aca30a8bd3c15c7d30541288bd7a2bdfcf
Simone Albanesi <simone.albanesi@edu.unito.it>
2024-10-09 18:24:37 +0200

    bugfix, added script for testing icc

--- PyART/catalogs/icc.py
+++ PyART/catalogs/icc.py
@@ -2,7 +2,8 @@ import os, json, re
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ..waveform    import Waveform#, WaveIntegrated
+from ..waveform    import Waveform 
+
 #from ..analysis.scattering_angle import ScatteringAngle
 from ..utils import os_utils as os_ut
 from ..utils import wf_utils as wf_ut
@@ -53,7 +54,8 @@ class Waveform_ICC(Waveform):
                 # leave only 150 M after merge
                 self.cut(DeltaT_end, from_the_end=True)
         else:
-            self.load_hlm()
+            self.load_hlm_compute_dothlm()
+
         
         pass
 
@@ -151,12 +153,16 @@ class Waveform_ICC(Waveform):
         self.r_extr  = self.extract_value(files[0], 'r')
         pass
     
-    def load_hlm(self):
+    def load_hlm_compute_dothlm(self):
+        """
+        Load hlm and compute dothlm
+        """
         dict_hlm, u, _ = self.load_multipole_txtfile('mp_strain', raise_error=False)
         self._u   = u
         self._hlm = dict_hlm
         if self.u is not None:
             self._u = self.u-self.u[0]
+        
         # compute dothlm
         dothlm = {}
         for k in self.hlm:
@@ -169,103 +175,7 @@ class Waveform_ICC(Waveform):
 ################################
 # Class for the ICC catalog
 ################################
-#class Catalog(object): #FIXME: old code, to FIX!
-#    def __init__(self, 
-#                 path        = './',
-#                 ID          = '0001',
-#                 ell_emms    = 'all',
-#                 ellmax      = 4,
-#                 nonspinning = False, # load only nonspinning
-#                 integr_opts = None,
-#                 load_puncts = True
-#                 ) -> None:
-#        simulations       = Simulations(path=basepath, icc_add_meta=True, metadata_ext='.bbh')
-#        self.catalog_meta = simulations.data
-#        self.nonspinning  = nonspinning
-#        self.integr_opts  = integr_opts
-#
-#        self.ellmax   = ellmax
-#        self.ell_emms = ell_emms
-#        if self.ell_emms == 'all': 
-#            self.modes = [(ell, emm) for ell in range(2,self.ellmax+1) for emm in range(-ell, ell+1)]
-#        else:
-#            self.modes = self.ell_emms # TODO: add check on input
-#        data = []
-#        for i, meta in enumerate(self.catalog_meta):
-#            spin_asum = abs(float(meta['initial-bh-spin1z'])) + abs(float(meta['initial-bh-spin2z']))
-#            if not self.nonspinning or spin_asum<1e-14:
-#                wave = WaveIntegrated(path=meta['path'], r_extr=meta['r0'], modes=self.modes, M=meta['M'],
-#                                      integr_opts=integr_opts)
-#                if load_puncts:
-#                    tracks = self.load_tracks(path=meta['path'])
-#                    punct0 = np.column_stack( (tracks['t'], tracks['t'], tracks['x0'], tracks['y0'], tracks['z0']) )
-#                    punct1 = np.column_stack( (tracks['t'], tracks['t'], tracks['x1'], tracks['y1'], tracks['z1']) )
-#                    scat_NR = ScatteringAngle(punct0=punct0, punct1=punct1, file_format='GRA', nmin=2, nmax=5, n_extract=4,
-#                                           r_cutoff_out_low=25, r_cutoff_out_high=None,
-#                                           r_cutoff_in_low=25, r_cutoff_in_high=100,
-#                                           verbose=False)
-#                    scat_info = {'chi':scat_NR.chi, 'chi_fit_err':scat_NR.fit_err}
-#                else:
-#                    tracks    = {}
-#                    scat_info = {}
-#                
-#                sim_data            = lambda:0
-#                sim_data.meta       = meta
-#                sim_data.wave       = wave
-#                sim_data.tracks     = tracks
-#                sim_data.scat_info  = scat_info
-#                data.append(sim_data)
-#        self.data = data
-#        pass
-#    
-#    def load_tracks(self, path):
-#        # FIXME: very specific
-#        fname = 'puncturetracker-pt_loc..asc'
-#        X  = np.loadtxt(os.path.join(path,fname))
-#        t  = X[:, 8]
-#        x0 = X[:,22]
-#        y0 = X[:,32]
-#        x1 = X[:,23]
-#        y1 = X[:,33]
-#        x  = x0-x1
-#        y  = y0-y1
-#        r  = np.sqrt(x**2+y**2)
-#        th = -np.unwrap(np.angle(x+1j*y))
-#        return {'t':t, 'x0':x0, 'x1':x1, 'y0':y0, 'y1':y1, 'r':r, 'th':th, 'z0':0*t, 'z1':0*t}
-#
-#    def get_simlist(self):
-#        """
-#        Get list of simulations' names
-#        """
-#        simlist = []
-#        for meta in self.catalog_meta:
-#            simlist.append(meta['name'])
-#        return simlist
-#
-#    def idx_from_value(self,value,key='name',single_idx=True):
-#        """ 
-#        Return idx with metadata[idx][key]=value.
-#        If single_idx is False, return list of indeces 
-#        that satisfy the condition
-#        """
-#        idx_list = []
-#        for idx, meta in enumerate(self.catalog_meta):
-#            if meta[key]==value:
-#                idx_list.append(idx)
-#        if len(idx_list)==0: 
-#            return None
-#        if single_idx:
-#            if len (idx_list)>1:
-#                raise RuntimeError(f'Found more than one index for value={value} and key={key}')
-#            else:
-#                return idx_list[0]
-#        else: 
-#            return idx_list
-#
-#    def meta_from_name(self,name):
-#        return self.catalog_meta[self.idx_from_value(name)]
-#    
-#    def wave_from_name(self,name):
-#        return self.waves[self.idx_from_value(name)]
-#
+#class Catalog(object):
+
+
 
---
+++ examples/icc.py
@@ -0,0 +1,79 @@
+import argparse
+import matplotlib.pyplot as plt
+from PyART.catalogs.icc import Waveform_ICC
+from PyART.analysis.integrate_multipole import Multipole
+from PyART.analysis.integrate_wave      import IntegrateMultipole
+import numpy as np
+
+path = '/Users/simonealbanesi/data/simulations_icc/ICCsims/catalog'
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--id', default=1,     type=int,     help='Simulatoion ID')
+parser.add_argument('--f0',       default=0.001, type=float,   help='FFI f0')
+parser.add_argument('--integration_test', action='store_true', help='Show integration test for new integrator')
+args = parser.parse_args()
+
+icc  = Waveform_ICC(path=path, ID=args.id)
+
+
+integr_opts = {'f0':args.f0, 'extrap_psi4':True, 'method':'FFI'}
+l = 2
+m = 2
+M = 1
+
+if args.integration_test:
+    t    = icc.t_psi4
+    psi4 = icc.psi4lm[(2,2)]['h'] 
+    dh   = icc.dothlm[(2,2)]['h'] 
+
+    #t         = icc.u
+    #signal    = dh    # psi4
+    #integrand = 'news' # 'psi4'
+    signal = psi4
+    integrand = 'psi4'
+    mode_new = IntegrateMultipole(l, m, t, signal, **integr_opts,
+                                  mass=M, radius=icc.r_extr, integrand=integrand)
+    #integr_opts['extrap_psi4'] = False
+    mode_old = Multipole(l, m, t, signal, mass=M, radius=icc.r_extr, integrand=integrand)
+    mode_old.hlm, mode_old.dothlm = mode_old.integrate_wave(integr_opts=integr_opts)
+
+    plt.figure(figsize=(10,6))
+    plt.subplot(3,2,1)
+    plt.plot(t, mode_old.hlm.real, c='r', ls='-',  label='old')
+    plt.plot(t, mode_new.h.real,   c='b', ls='--', label='new')
+    plt.legend()
+    plt.subplot(3,2,2)
+    plt.plot(t, np.abs(mode_old.hlm - mode_new.h), c='g', label='diff')
+    plt.yscale('log')
+    plt.legend()
+    plt.subplot(3,2,3)
+    plt.plot(t, mode_old.dothlm.real, c='r', ls='-')
+    plt.plot(t, mode_new.dh.real,     c='b', ls='--')
+    plt.subplot(3,2,4)
+    plt.plot(t, np.abs(mode_old.dothlm - mode_new.dh), c='g')
+    plt.subplot(3,2,5)
+    plt.plot(t, mode_old.psi.real,  c='r', ls='-')
+    plt.plot(t, mode_new.psi4.real, c='b', ls='--')
+    plt.subplot(3,2,6)
+    plt.plot(t, np.abs(mode_old.psi - mode_new.psi4), c='g')
+    plt.show()
+
+iicc = Waveform_ICC(path=path, ID=args.id, integrate=True, 
+                    integr_opts={'f0':args.f0, 'window':[20,-20]})
+
+for k in icc.metadata:
+    print(f'{k:10s} : {icc.metadata[k]}')
+
+icc_tmrg, _, _, _  =  icc.find_max()
+iicc_tmrg, _, _, _ = iicc.find_max()
+
+plt.figure(figsize=(9,6))
+plt.plot( icc.u -  icc_tmrg,  icc.hlm[(2,2)]['A'],    lw=0.5, c='r', label='stored')
+plt.plot( icc.u -  icc_tmrg,  icc.hlm[(2,2)]['real'], lw=1.0, c='r', label=None)
+plt.plot( icc.u -  icc_tmrg,  icc.hlm[(2,2)]['imag'], lw=1.0, c='r', ls=':', label=None)
+plt.plot(iicc.u - iicc_tmrg, iicc.hlm[(2,2)]['A'],    lw=0.5, c='b', label='integrated')
+plt.plot(iicc.u - iicc_tmrg, iicc.hlm[(2,2)]['real'], lw=1.0, c='b', label=None)
+plt.plot(iicc.u - iicc_tmrg, iicc.hlm[(2,2)]['imag'], lw=1.0, c='b', ls=':', label=None)
+plt.legend()
+plt.show()
+
+
