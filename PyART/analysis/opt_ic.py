760a1f02d5a15d29602b9371c9eea2b001895d5f
Simone Albanesi <simone.albanesi@edu.unito.it>
2024-12-17 17:31:55 +0100

    commented debug print-statement

--- PyART/analysis/opt_ic.py
+++ PyART/analysis/opt_ic.py
@@ -396,7 +396,7 @@ class Optimizer(object):
             #eob_wave._u = eob_wave.u#-eob_wave.u[0]
         except Exception as e:
             # FIXME: no error msg is a little bit criminal
-            print(f'Error occured in EOB wave generation:\n{e}')
+            #print(f'Error occured in EOB wave generation:\n{e}')
             eob_wave = None
         return eob_wave
     
