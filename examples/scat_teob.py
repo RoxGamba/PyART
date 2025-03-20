3de3e5ea2cc856f82921a20a0683b36f8a446189
Simone Albanesi <simone.albanesi@edu.unito.it>
2025-03-20 15:58:43 +0100

    minor

--- examples/scat_teob.py
+++ examples/scat_teob.py
@@ -5,11 +5,11 @@ from PyART.utils.utils import D1
 import numpy as np
 
 configurations = [
-    {'b': 9.68, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
-    {'b':10.00, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
-    {'b':11.00, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
-    {'b':12.00, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
-    {'b':13.00, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
+    {'b': 9.678, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
+    {'b':10.000, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
+    {'b':11.000, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
+    {'b':12.000, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
+    {'b':13.000, 'chi1':0., 'chi2':0., 'E0':1.0226, 'p':0.11456439, 'eob':None, 'scat':None},
 ]             
 n_conf = len(configurations)
 
@@ -24,13 +24,20 @@ for i in range(n_conf):
     eobpars = teob.CreateDict(r_hyp=r0, H_hyp=conf['E0'], J_hyp=J0, q=1, 
                              chi1z=conf['chi1'], chi2z=conf['chi2'])
     eob = teob.Waveform_EOB(pars=eobpars)
-    
-    scat = ScatteringAngle(puncts=eob.dyn, nmin=2, nmax=10, n_extract=None,
-                           hypfit=True, hypfit_plot=False,
-                           r_cutoff_in_low=cutoff_min,  r_cutoff_in_high=eob.dyn['r'][0],
-                           r_cutoff_out_low=cutoff_min, r_cutoff_out_high=None, verbose=True)
     conf['eob']  = eob
-    conf['scat'] = scat
+    E_final = eob.dyn['E'][-1]
+    print(f"E0, pph0         : {conf['E0']:.5f}, {J0:.5f}")
+    print(f"Final energy     : {E_final:.5f}")
+    
+    if E_final>1:
+        scat = ScatteringAngle(puncts=eob.dyn, nmin=2, nmax=10, n_extract=None,
+                               hypfit=True, hypfit_plot=False,
+                               r_cutoff_in_low=cutoff_min,  r_cutoff_in_high=eob.dyn['r'][0],
+                               r_cutoff_out_low=cutoff_min, r_cutoff_out_high=None, verbose=True)
+        conf['scat'] = scat
+    else:
+        print('Capture!')
+
     print(' ')
 del eob, scat
 
@@ -41,7 +48,9 @@ for conf in configurations:
     
     eob  = conf['eob']
     scat = conf['scat']
-
+    
+    if scat is None:
+        continue
     plt.subplot(2,2,1)
     plt.plot(eob.u, eob.hlm[(2,2)]['real'])
     plt.xlim([-100, 100]) 
@@ -52,7 +61,7 @@ for conf in configurations:
     b      = conf['b']
     chi_BH = conf['chi1']
     plt.subplot(2,2,2)
-    plt.plot(eob.u, -psi4.real, label=f'{b:.1f}, {chi_BH:3}')
+    plt.plot(eob.u, -psi4.real, label=f'{b:.3f}, {chi_BH:3}')
     plt.xlim([-100, 100]) 
     plt.legend()
 
