a2f95ebc6c7771f9e64f93221160781b0f6daaba
RoxGamba <rgamba@berkeley.edu>
2025-04-26 23:21:40 -0400

    Add two simple tests

---
+++ tests/test_waveform.py
@@ -0,0 +1,21 @@
+"""
+Generial tests for the waveform class in PyART
+"""
+
+from PyART import waveform
+
+import numpy as np
+
+def test_waveform():
+
+    # Create a mock waveform
+    wf = waveform.Waveform()
+
+    # check that it has the right attributes
+    for attr in ['hlm', 'u', 't', 'f', 'hp', 'hc', 'dothlm', 'psi4lm', 'dyn', 'kind']:
+        assert hasattr(wf, attr), f"Waveform object does not have attribute {attr}"
+    
+    # check methods
+    # ...
+
+    pass
