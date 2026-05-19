5c06d7f1b20a8dd99d48959a2d040c0f983c273c
Simone Albanesi <simone.albanesi@edu.unito.it>
2026-05-19 17:48:35 +0200

    fixes for phenom class

--- PyART/models/phenom.py
+++ PyART/models/phenom.py
@@ -1,5 +1,4 @@
 from ..waveform import Waveform
-from phenomxpy.utils import MasstoSecond, SecondtoMass, AmpNRtoSI
 import numpy as np
 from PyART.utils.wf_utils import get_multipole_dict
 
@@ -14,65 +13,28 @@ class Waveform_IMRPhenomT(Waveform):
     Interface for IMRPhenomT via phenomxpy
     """
 
-    def __init__(self, pars=None, approx="IMRPhenomTHM", kind="TD"):
+    def __init__(self, pars=None, approx="IMRPhenomTHM"):
         super().__init__()
         self.pars = pars
         self.approx = approx
-        self._kind = kind
         self._run()
         pass
 
     # ---------------------------
-    # Parameter conversion
-    # ---------------------------
-    def _build_params(self):
-        """
-        Assume that pars of self contains:
-            - q                   : mass ratio
-            - M                   : total mass
-            - chi1x, chi1y, chi1z : spin components of body 1
-            - chi2x, chi2y, chi2z : spin components of body 2
-            - distance            : distance to source
-            - inclination         : inclination
-            - coalescence_angle   : reference phase
-            - initial frequency   : initial frequency
-            - srate_interp        : sampling rate
-            - use_mode_lm         : list of modes to use for hpc
-        """
-        # Read in from pars the relevant parameters
-        pp = self.pars
-        q = pp["q"]
-        c1x, c1y, c1z = pp["chi1x"], pp["chi1y"], pp["chi1z"]
-        c2x, c2y, c2z = pp["chi2x"], pp["chi2y"], pp["chi2z"]
-        flow = pp["initial_frequency"]
-
-        nu = q / (1.0 + q)**2
-
-        params = dict(
-            eta = nu,
-            s1 = [c1x,c1y,c1z],
-            s2 = [c2x,c2y,c2z],   
-            f_min = flow,    
-            delta_t = 0.5,
-        )
-
-        return params
-
-    # ---------------------------
     # Approximant selector
     # ---------------------------
-    def _get_approximant(self, params):
+    def _get_approximant(self, pars):
         if self.approx == "IMRPhenomT":
-            return IMRPhenomT(**params)
+            return IMRPhenomT(**pars)
 
         elif self.approx == "IMRPhenomTHM":
-            return IMRPhenomTHM(**params)
+            return IMRPhenomTHM(**pars)
 
         elif self.approx == "IMRPhenomTP":
-            return IMRPhenomTP(**params)
+            return IMRPhenomTP(**pars)
 
         elif self.approx == "IMRPhenomTPHM":
-            return IMRPhenomTPHM(**params)
+            return IMRPhenomTPHM(**pars)
 
         else:
             raise ValueError("Unknown approximant")
@@ -81,20 +43,9 @@ class Waveform_IMRPhenomT(Waveform):
     # Main dispatcher
     # ---------------------------
     def _run(self):
-        params = self._build_params()
-        phen = self._get_approximant(params)
-        
-        if self.kind == "THM":
-            self._run_THM(phen)
-
-        elif self.kind == "TPHM":
-            self._run_TPHM(phen)
-
-        elif self.kind == "FD":
-            self._run_FD(phen)
-
-        else:
-            raise ValueError("kind must be THM, TPHM or FD")
+        phen = self._get_approximant(self.pars)
+        self._run_THM(phen)
+        pass        
 
     # ---------------------------
     # Time domain
@@ -104,64 +55,92 @@ class Waveform_IMRPhenomT(Waveform):
         # Compute THM polarizations
         hp, hc, t = phen.compute_polarizations(times=None)
         
-        # Compute THM individual modes
-        hlms, t = phen.compute_hlms(times=None)
-
-        hlms_pyart = {}
-        for ky in hlms:
-            l = int(ky[0])
-            m = int(ky[1:])
-            z = hlms[ky]
-            hlms_pyart[(l,m)] = get_multipole_dict(z)
-
-
-        self._u = t
+        self._u  = t
         self._hp = np.array(hp)
         self._hc = np.array(hc)
-
-        self.hlms_pyart = hlms_pyart
-
-        pass
-
-    def _run_TPHM(self, phen):
-
-        # Compute TPHM polarizations
-        hp, hc, t = phen.compute_polarizations(times=None)
         
-        # Compute TPHM individual modes
-        hlms, t = phen.compute_CPmodes(times=None)
+        # Compute THM individual modes
+        if self.approx[-2:]=='HM':
+            if 'PhenomTP' in self.approx:
+                hlm_phen, tlm = phen.compute_CPmodes(times=None)
+            else:
+                hlm_phen, tlm = phen.compute_hlms(times=None)
 
-        hlms_pyart = {}
-        for ky in hlms:
-            l = int(ky[0])
-            m = int(ky[1:])
-            z = hlms[ky]
-            hlms_pyart[(l,m)] = get_multipole_dict(z)
+            hlm = {}
+            for ky in hlm_phen: # keys: '22', '21', etc.
+                l = int(ky[0])
+                m = int(ky[1:])
+                z = hlm_phen[ky]
+                hlm[(l,m)] = get_multipole_dict(z)
 
-
-        self._u = t
-        self._hp = np.array(hp)
-        self._hc = np.array(hc)
-
-        self.hlms_pyart = hlms_pyart
-
+            self._hlm = hlm
         pass
 
-    # ---------------------------
-    # Frequency domain
-    # ---------------------------
-    def _run_FD(self, phen):
-        # Creation time array
-        #srate = self.pars["srate_interp"]
-        #duration = self.pars.get("duration", 1.0)
+#    # ---------------------------
+#    # Frequency domain
+#    # ---------------------------
+#    def _run_FD(self, phen):
+#        # Creation time array
+#        #srate = self.pars["srate_interp"]
+#        #duration = self.pars.get("duration", 1.0)
+#        
+#        # Compute FD polarizations
+#        hpf, hcf, f = phen.compute_fd_polarizations(times=None)
+#
+#        #Calcolo HMs?
+#
+#        self._f = np.array(f)
+#        self._hp = np.array(hpf)
+#        self._hc = np.array(hcf)
 
-        #t = MasstoSecond(phen.set_time_array(), phen.pWF.total_mass)
+def CreateDict(
+    q=1.,
+    chi1z=0.,
+    chi2z=0.,
+    chi1x=0.,
+    chi2x=0.,
+    chi1y=0.,
+    chi2y=0.,
+    f0=0.0035,
+    dt=0.5,
+):
+    """
+    Create the dictionary of parameters for IMRPhenomT (geom units)
+    
+    Parameters
+    ----------
+    q : float
+        Mass ratio m1/m2 >= 1. Default is 1.
+    chi1z : float
+        Dimensionless spin of the primary along the orbital angular momentum.
+        Default is 0.0.
+    chi2z : float
+        Dimensionless spin of the secondary along the orbital angular momentum.
+        Default is 0.0.
+    chi1x : float
+        Dimensionless spin of the primary in the orbital plane (x-component).
+        Default is 0.0.
+    chi2x : float
+        Dimensionless spin of the secondary in the orbital plane (x-component).
+        Default is 0.0.
+    chi1y : float
+        Dimensionless spin of the primary in the orbital plane (y-component).
+        Default is 0.0.
+    chi2y : float
+        Dimensionless spin of the secondary in the orbital plane (y-component).
+        Default is 0.0.
+    f0 : float
+        Starting frequency of the (2,2) mode in geometric units. Default is 0.0035.
+    dt : float
+        Time step in seconds. Default is 0.5
+    """
+    pars = {
+        "eta"     : q/(1+q)**2, 
+        "s1"      : [chi1x,chi1y,chi1z],
+        "s2"      : [chi2x,chi2y,chi2z],
+        "f_min"   : f0,
+        "delta_t" : dt,
+    }
+    return pars
 
-        # Compute FD polarizations
-        hpf, hcf, f = phen.compute_fd_polarizations(times=None)
 
-        #Calcolo HMs?
-
-        self._f = np.array(f)
-        self._hp = np.array(hpf)
-        self._hc = np.array(hcf)
